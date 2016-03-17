013d50f8063b532fdc83b3c546faf8aaad316567
hulei02 <hulei02@meituan.com>
2016-03-17 20:53:09 +0800

    refactor structure

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -1,9 +1,11 @@
-package hello;
+package controller;
 
-import java.util.concurrent.atomic.AtomicLong;
+import model.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
