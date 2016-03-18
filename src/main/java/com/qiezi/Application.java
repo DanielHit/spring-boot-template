4632b36533a01367819ba833517cd79005f71b01
hulei02 <hulei02@meituan.com>
2016-03-18 16:22:12 +0800

    add appliation properties

--- src/main/java/com/qiezi/Application.java
+++ src/main/java/com/qiezi/Application.java
@@ -2,10 +2,16 @@ package com.qiezi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@Configuration
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/qiezi/controller/IndexController.java
+++ src/main/java/com/qiezi/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.qiezi.controller;
 
 import com.qiezi.model.Greeting;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetingController {
+public class IndexController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello-what, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @Value("${username}")
+    private String userName;
+
+    @RequestMapping("/index")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                String.format(template, userName));
     }
 
 }
